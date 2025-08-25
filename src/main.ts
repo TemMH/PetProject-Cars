31c7a1211ea3da4a45b0c548d070ab1ffe413836
Artemiy Levkevich <a_gakh1@mail.ru>
2025-08-25 21:05:12 +0800

    Develop version

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Cars } from './app/cars/cars';
+
+@Component({
+  selector: 'app-root',
+  imports: [Cars],
+  template: `<app-cars></app-cars>`,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
